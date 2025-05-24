21c1d651ce3b21f907308a4dd20445b353e41ce9
AlarconMarisel <marisel.alarcon46@gmail.com>
2025-05-24 11:29:40 -0300

    Modificadion en actividades

--- Actividad.cpp
+++ Actividad.cpp
@@ -14,11 +14,42 @@ Actividad::Actividad(){
 
 }
 
-    Actividad(int idActividad, const char* nombre, const char* diaDeLaSemana, int cuposDisponibles, Horario horarioActividad, Tutor idTutorACargo);
-    void setIdActividad(int idActividad);
-    int getIdActividad();
-    void setNombre(const char* nombre);
-    const char* getNombre();
+Actividad::Actividad(int idActividad, const char* nombre, const char* diaDeLaSemana, int cuposDisponibles, Horario horarioActividad, Tutor idTutorACargo){
+
+    setIdActividad(idActividad);
+    setNombre(nombre);
+    setDiaDeLaSemana(diaDeLaSemana);
+    setCuposDisponibles(cuposDisponibles);
+    setHorarioActividad(horarioActividad);
+    setIdTutorACargo(idTutorACargo);
+
+
+}
+void Actividad::setIdActividad(int idActividad){
+
+    _idActividad=idActividad;
+
+
+}
+int Actividad::getIdActividad(){
+
+    return _idActividad;
+
+}
+void Actividad::setNombre(const char* nombre){
+
+    if (strlen(nombres) > 0) {
+        strcpy(_nombres, nombres);
+    } else {
+        strcpy(_nombres, "SinNombre");
+    }
+
+}
+const char* Actividad::getNombre(){
+
+    return _nombre;
+
+}
     void setDiaDeLaSemana(const char* diaDeLaSemana);
     const char* getDiaDeLaSemana();
     void setCuposDisponibles(int cuposDisponibles);
